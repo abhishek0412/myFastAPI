ddbcf5dc33d96a250b920315dde73157ccab5874
abhishek choudhary <abhishek.choudharry@gmail.com>
2023-04-25 16:21:24 +0530

    Added two more get calls

--- myAPI/main.py
+++ myAPI/main.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
+# all the get call
 @app.get("/")
 async def root():
     return {"message": "Hello World! This is my first fatAPI"}
@@ -11,3 +13,12 @@ async def root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
