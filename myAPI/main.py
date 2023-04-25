8e171e9b98fdb793e40b96962d3f3217e9cdb626
abhishek choudhary <abhishek.choudharry@gmail.com>
2023-04-25 16:17:56 +0530

    Added path parameters with datatypes

--- myAPI/main.py
+++ myAPI/main.py
@@ -7,3 +7,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World! This is my first fatAPI"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
